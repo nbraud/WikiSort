ce3df5d26a1b22627fa01bc2a9516e69095f70b6
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2014-03-20 20:30:21 +0100

    bench: Use a random permutation for benchmarking

--- bench.ml
+++ bench.ml
@@ -1,12 +1,22 @@
-open WikiSort;;
+open WikiSort
 
-let n = 100_000 in
-let l = Array.init n (fun i -> n-i) in
-Benchmark.latencyN 500L ["merge_sort", merge_sort Pervasives.compare, l;
-						 "Array.sort", Array.sort Pervasives.compare, l]
+let () = Random.init 424242
+
+let shuffle t =
+  for i=1 to Array.length t -1 do
+	swap t i @@ Random.int (i+1)
+  done
+
+let _ =
+  let n = 100_000 in
+  let l = Array.make n (fun i -> i) in
+  shuffle l;
+  Benchmark.latencyN 500L
+	["merge_sort", merge_sort Pervasives.compare, Array.copy l;
+	 "Array.sort", Array.sort Pervasives.compare, Array.copy l]
 
 (* Running with -unsafe (un-guarded array access)
-      Latencies for 100 iterations of "merge_sort", "Array.sort":
-merge_sort: 12.53 WALL (12.49 usr +  0.03 sys = 12.51 CPU) @  7.99/s (n=100)
-Array.sort: 39.02 WALL (38.91 usr +  0.06 sys = 38.96 CPU) @  2.57/s (n=100)
+   Latencies for 500 iterations of "merge_sort", "Array.sort":
+merge_sort:  1.43 WALL ( 1.42 usr +  0.00 sys =  1.43 CPU) @ 350.45/s (n=500)
+Array.sort: 23.00 WALL (22.94 usr +  0.04 sys = 22.98 CPU) @ 21.76/s (n=500
 *)
